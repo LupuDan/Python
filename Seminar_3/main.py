6acaf88c20deed0e8b7300a391efe476d7159dfa
Lupu Daniel <dlupu666@gmail.com>
2023-05-30 10:43:22 +0300

    Task_2

--- Seminar_3/main.py
+++ Seminar_3/main.py
@@ -5,3 +5,20 @@
 # print(len(set(list_nums)))
 
 
+# Дана последовательность из N целых чисел и число K.
+# Необходимо сдвинуть всю последовательность
+# (сдвиг - циклический) на K элементов вправо,
+# K – положительное число.
+
+
+# list_nums = [1, 2, 3, 4, 5]
+
+# num = int(input())
+# n = len(list_nums)
+
+# for i in range(0, num % n):
+#     list_nums.insert(0, list_nums.pop(-1))
+
+# print(list_nums)
+
+
