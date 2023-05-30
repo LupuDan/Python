743245ced82e62893f994194b481374b5ae28c59
Lupu Daniel <dlupu666@gmail.com>
2023-05-30 10:56:57 +0300

    Task_2

--- Seminar_3HW/main.py
+++ Seminar_3HW/main.py
@@ -2,12 +2,42 @@
 # Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
 # В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
 
-def count_occurrences(array, x):
-    count = 0
+# def count_occurrences(array, x):
+#     count = 0
+#     for num in array:
+#         if num == x:
+#             count += 1
+#     return count
+
+# N = int(input("Введите количество элементов в массиве: "))
+
+# array = []
+# for i in range(N):
+#     element = int(input("Введите элемент массива: "))
+#     array.append(element)
+
+# X = int(input("Введите число X: "))
+
+# occurrences = count_occurrences(array, X)
+
+# print("Число", X, "встречается", occurrences, "раз(а) в массиве.")
+
+
+# Задача 18: Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X.
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. В последующих  строках записаны N целых чисел Ai.
+# Последняя строка содержит число X
+
+def find_closest_element(array, x):
+    closest_element = array[0]
+    min_difference = abs(x - array[0])
+    
     for num in array:
-        if num == x:
-            count += 1
-    return count
+        difference = abs(x - num)
+        if difference < min_difference:
+            min_difference = difference
+            closest_element = num
+    
+    return closest_element
 
 N = int(input("Введите количество элементов в массиве: "))
 
@@ -18,6 +48,6 @@ for i in range(N):
 
 X = int(input("Введите число X: "))
 
-occurrences = count_occurrences(array, X)
+closest_element = find_closest_element(array, X)
 
-print("Число", X, "встречается", occurrences, "раз(а) в массиве.")
+print("Самый близкий элемент к числу", X, "в массиве:", closest_element)
